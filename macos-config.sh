c4ef3c1ed340528a9f6a2c3b9ec60ea8057295a1
Jon Brooks <thomas.brooks@mheducation.com>
2017-03-14 22:19:33 -0700

    More aliases and functions.  Mac setup script

---
+++ macos-config.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
