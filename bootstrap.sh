4bb26420eeb7c84447015ec0fe964f59991fdeb1
Jon Brooks <thomas.brooks@mheducation.com>
2017-03-15 08:41:07 -0700

    backup orig files in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,12 @@
 
 CUR_DIR=`pwd`
 
+#backup original files
+mkdir ~/dotfile-backups
+mv ~/.atom ~/dotfile-backups/.
+mv ~/.bash_profile ~/dotfile-backups/.
+mv ~/.inputrc ~/dotfile-backups/.
+
 ln -s "$CUR_DIR"/atom ~/.atom
 ln -s "$CUR_DIR"/bash_profile ~/.bash_profile
 ln -s "$CUR_DIR"/inputrc ~/.inputrc
